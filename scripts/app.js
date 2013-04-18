487e30c25df2cd91ac0ed30bc862b2407f87f94d
Timo Uhlmann <t.uhlmann@zeroseven.de>
2013-04-18 17:23:56 +0200

    grunt-browserify -- still duplicate Buffer and process

---
+++ scripts/app.js
@@ -0,0 +1,42 @@
+var Db = require('mongodb').Db
+  , Connection = require('mongodb').Connection
+  , Server = require('mongodb').Server
+  , format = require('util').format;
+
+var host = '127.0.0.1';
+var port = Connection.DEFAULT_PORT;
+
+console.log('Connecting to ' + host + ':' + port);
+Db.connect(format('mongodb://%s:%s/node-mongo-examples?w=1', host, port), function(err, db) {
+  console.log('callback', err, db);
+  db.dropDatabase(function(err, result) {
+    db.collection('test', function(err, collection) {
+      // Erase all records from the collection, if any
+      collection.remove({}, function(err, result) {
+        // Insert 3 records
+        for(var i = 0; i < 3; i++) {
+          collection.insert({'a':i}, {w:0});
+        }
+        
+        collection.count(function(err, count) {
+          console.log("There are " + count + " records in the test collection. Here they are:");
+
+          collection.find().each(function(err, item) {
+            if(item != null) {
+              console.dir(item);
+              console.log("created at " + new Date(item._id.generationTime) + "\n")
+            }
+
+            // Null signifies end of iterator
+            if(item == null) {                
+              // Destory the collection
+              collection.drop(function(err, collection) {
+                db.close();
+              });
+            }
+          });
+        });
+      });      
+    });
+  });
+});
